2faddf7668793fcb4325ea0f01d52c22cd15739b
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-23 07:01:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ class Formatter {
 */
 
   static capitalize(str){
-    let capitalize = str.charAt(0).toUpperCase + str.slice(1);
+    let capitalize = str.charAt(0).toUpperCase() + str.slice(1);
     capitalize;
   }
   static sanitize(str){
